1d9f307bf700b07ed9620076c02861096c2fe641
qkrwogk110 <qkrwogk110@naver.com>
2018-01-20 16:46:50 +0900

    stream

--- src/app.js
+++ src/app.js
@@ -2,26 +2,10 @@ const openStream = require('./openStream');
 
 //openStream();
 
-const $ = require('jquery');
+openStream();
+//p.on('connect', () => {
+//      setInterval( () => p.send(Math.random()),2000);
+//});
 
-const Peer = require('simple-peer');
-
-const p = new Peer({ initiator: location.hash === '#1', trickle: false });
-
-p.on('signal', token => {
-    $('#txtMySignal').val(JSON.stringify(token))
-});
-
-p.on('connect', () => {
-      setInterval( () => p.send(Math.random()),2000);
-});
-
-p.on('data',data => console.log('NHAN DU LIEU: ' + data));
-
-
-$('#btnConnect').click( () => {
-    const friendSignal = JSON.parse($('#txtFriendSignal').val());
-    p.signal(friendSignal);
-});
-
-console.log('Xin chao cac ban');
+//p.on('data',data => console.log('NHAN DU LIEU: ' + data));
+//console.log('Xin chao cac ban');
--- src/openStream.js
+++ src/openStream.js
@@ -1,8 +1,26 @@
 const playVideo = require('./playVideo');
+const Peer = require('simple-peer');
+const $ = require('jquery');
+
 
 function openStream(){
   navigator.mediaDevices.getUserMedia({ audio: false, video: true})
-  .then(stream => playVideo(stream, 'localStream'))
+  .then(stream => { playVideo(stream, 'localStream')
+
+  const p = new Peer({ initiator: location.hash === '#1', trickle: false ,stream});
+
+  p.on('signal', token => {
+      $('#txtMySignal').val(JSON.stringify(token))
+  });
+
+  $('#btnConnect').click( () => {
+      const friendSignal = JSON.parse($('#txtFriendSignal').val());
+      p.signal(friendSignal);
+  });
+
+  p.on('stream', friendStream => playVideo(friendStream,'friendStream'));
+
+})
   .catch(err => console.log(err));
 }
 
